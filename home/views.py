1ccd6e86251533907029757138dc439eff0b42e6
Simone <rexjaa@gmail.com>
2018-04-24 22:35:48 +0200

    barra completata

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect
+
 
 @login_required(login_url='/authentication/login/')
 def index(request):
