3482a80458e74c1f781eeb7f0a63765a522aa1fc
Simone <rexjaa@gmail.com>
2018-05-02 12:35:52 +0200

    barra laterale

--- graphsite/settings.py
+++ graphsite/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+]
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/home/'
--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_protect
 
 
 @login_required(login_url='/authentication/login/')
 def index(request):
     return render(request, 'home/index.html', {})
+
