60f015aaa8e0f5fd95ffb243eea36f52c60fd8bd
brofjst <rexaa@gmail.com>
2018-09-26 18:40:58 +0200

    bug user button

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render
 from management.models import Dipendente
-
+from django.shortcuts import render_to_response
 
 @login_required(login_url='/authentication/login/')
 def index(request):
@@ -10,12 +10,20 @@ def index(request):
         search_user = request.GET.get('searchuser', "").upper()
         search_role = request.GET.get('searchrole', "").upper()
         search_city = request.GET.get('searchcity', "").upper()
+        result = {}
+
+        if search_user == "" and search_role == "" and search_city == "":
+            return render(request, 'home/index.html', {})
+
+        if not checkinput(search_user, search_role, search_city):
+            result['Error'] = "item not in database"
+            return render_to_response('home/index.html', {'result': result})
 
         try:
             query = Dipendente.objects.all()
 
         except:
-            print("not in database")
+            print("Error reading the database")
 
         # rimuovo dalla query tutto ciò che non matcha grazie a Q che opera da NOT
         else:
@@ -29,10 +37,36 @@ def index(request):
                 query = query.exclude(~Q(sede=search_city))
 
         # inserisco i risultati in un dizionario
-        result = {}
         for k in query:
             result[k] = k.ruolo, k.sede
 
-        return render(request,'home/index.html', result)
+        return render_to_response('home/index.html', {'result': result})  # passo il dizionario al template
 
     return render(request, 'home/index.html', {})
+
+
+# controllo che i dati inseriti siano presenti nel database
+def checkinput(search_user, search_role, search_city):
+    if search_user != "":
+        try:
+            Dipendente.objects.get(nick=search_user)
+        except:
+            return False
+
+    if search_role != "":
+        try:
+            check = Dipendente.objects.filter(ruolo=search_role)
+        except:
+            print("error reading db")
+        if not check:  # se il queryset è vuoto la stringa inserita è errata
+            return False
+
+    if search_city != "":
+        try:
+            check = Dipendente.objects.filter(sede=search_city)
+        except:
+            print("error reading db")
+        if not check:
+            return False
+
+    return True
